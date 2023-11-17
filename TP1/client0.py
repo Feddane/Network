fdacf1a18aedaafa4602bdd84a819cde71e09a46
Feddane <chafeddane@gmail.com>
2023-11-17 10:49:33 +0100

    comment the TP1

--- TP1/client0.py
+++ TP1/client0.py
@@ -2,12 +2,13 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#definition de l'adresse et du port du serveur auquel le client tente de se connecter
 host, port =  "127.0.0.1", 9999
 
 try:
     client.connect((host, port))
-    print("client connected")
+    print("client connecté")
 except:
-    print("connection to server failed")
+    print("connection au serveut echouée !!!")
 finally:
     client.close()
--- TP1/server0.py
+++ TP1/server0.py
@@ -1,16 +1,19 @@
 import socket
 
+# Création du socket du serveur, SOCK_STREAM = pour les connexions TCP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#Définition de l'adresse et du port du serveur
 host, port =  "127.0.0.1", 9999
 
+#liaison du serveur à l'adresse et au port spécifiés
 server.bind((host, port))
-print("Server started")
+print("Le serveur est démarré")
 
 while True:
-    server.listen(4)
+    server.listen(4) # le serveur peut accepter jusqu'à 4 connexions en attente.
     conn, address = server.accept()
-    print("Connection listening  ")
+    print("Serveur en écoute  ")
 
     conn.close()
 server.close()
