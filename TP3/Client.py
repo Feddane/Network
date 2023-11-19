2bdf1b84c89a385e654d82734a3404555dcf1289
Feddane <chafeddane@gmail.com>
2023-11-19 20:39:47 +0100

    last

--- TP3/Client.py
+++ TP3/Client.py
@@ -2,54 +2,45 @@ import socket
 import threading
 import  os
 
-# Fonction pour gérer la réception des messages du serveur
+
 def receive_messages(client_socket):
     try:
         while True:
-            message = client_socket.recv(128).decode('utf-8') #decode le message et le mettre dans message
-            if not message:  #message vide
+            message = client_socket.recv(128).decode('utf-8')
+            if not message:
                 break
             print(message)
-    except Exception as e:  #si une connexion a ete interompue ou si on execute en premier le fichier client
+    except Exception as e:
         print(f"Erreur de réception : {e}")
     finally:
         client_socket.close()
         print("La connexion au serveur a été fermée.")
-        os._exit(0) #terminer le programme de manière brutale
+        os._exit(0)
 
 
-# Création du socket client
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Définition de l'adresse et du port du serveur
+
 host, port = "127.0.0.1", 9999
 
-# Tentative de connexion au serveur
+
 client.connect((host, port))
 
-# Demande du nom de l'utilisateur
+
 nom = input('Quel est votre nom ? ')
 
-# Envoi du nom au serveur
+
 client.send(f"{nom} a rejoint le chatroom.".encode('utf-8'))
 
-# Création d'un thread pour recevoir les messages en parallèle
-thread_receive = threading.Thread(target=receive_messages, args=(client,)) #Cela crée un nouvel objet de thread appelé thread_receive. L'argument target spécifie la fonction que le thread exécutera, et args spécifie les arguments à passer à cette fonction. Dans ce cas, le thread exécutera la fonction receive_messages et passera le socket du client client comme argument.
-thread_receive.start()
-    #En résumé, ces trois lignes de code créent un thread (thread_receive) 
-    # qui exécute la fonction receive_messages en arrière-plan. 
-    # Cela permet au programme principal de continuer son exécution, 
-    # notamment d'envoyer des messages depuis l'utilisateur, sans être bloqué par 
-    # la fonction de réception des messages du serveur. 
-    # Le thread créé s'occupe de manière asynchrone de la réception des messages du serveur 
-    # tout en permettant à l'utilisateur d'interagir avec l'interface utilisateur principale de l'application.
 
-# Boucle principale pour envoyer des messages
+thread_receive = threading.Thread(target=receive_messages, args=(client,))
+thread_receive.start()
+
+
 while True:
     message = input(f"{nom} > ")
     client.send(f"{nom} > {message}".encode('utf-8'))
 
 
 
-
-#thread == L'utilisation de threads permet d'accomplir plusieurs tâches en parallèle, sans bloquer l'exécution du programme
--- TP3/Serveur.py
+++ TP3/Serveur.py
@@ -1,30 +1,30 @@
 import socket
 import select
 
-# Création du socket serveur
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Définition de l'adresse et du port
+
 host, port = "127.0.0.1", 9999
 
-# Liaison du serveur à l'adresse et au port spécifiés
-server.bind((host, port))
-server.listen(5)  # Permet jusqu'à 5 connexions en attente
 
-# Liste des sockets (serveur inclus) pour la gestion avec select
+server.bind((host, port))
+server.listen(5)
+
+
 socket_objs = [server]
 
 print("Bienvenue dans le chatroom !!!")
 
-# Fonction pour diffuser un message à tous les clients
+
 def broadcast(server_socket, sender_socket, message):
     for socket_obj in socket_objs:
-        # Envoyer le message à tous les clients, sauf à l'expéditeur et au serveur
-        if socket_obj != server_socket and socket_obj != sender_socket:
+
+        if  socket_obj != server_socket and socket_obj != sender_socket:
             try:
                 socket_obj.send(message.encode('utf-8'))
             except:
-                # Gérer les erreurs d'envoi (peut se produire si le client est déconnecté) pour pas que le client envoi des msg a des clients deconnectes
+
                 socket_objs.remove(socket_obj)
 
 while True:
@@ -33,22 +33,20 @@ while True:
 
     for socket_obj in list_lue:
         if socket_obj is server:
-            # Nouvelle connexion entrante
+
             client, address = server.accept()
             socket_objs.append(client)
             print(f"Nouveau participant connecte depuis {address}")
             broadcast(server, client, f"{address} a rejoint le chatroom.")
         else:
-            # Réception de données d'un client connecté
             try:
                 donnees_recus = socket_obj.recv(128).decode('utf-8')
 
                 if donnees_recus:
                     print(donnees_recus)
-                    # Diffuser le message à tous les clients
                     broadcast(server, socket_obj, donnees_recus)
             except ConnectionResetError:
-                # Le client s'est déconnecté
+
                 socket_objs.remove(socket_obj)
                 print(f"Un participant est deconnecte")
                 broadcast(server, socket_obj, "Un participant s'est deconnecte.")
