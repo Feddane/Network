dfed6da2473afd6bf9fefc8c311188409055dadf
Feddane <chafeddane@gmail.com>
2023-11-22 10:19:40 +0100

    comment the: add the name of the client when he's disconnecting

--- TP3/Client.py
+++ TP3/Client.py
@@ -1,47 +1,52 @@
 import socket
 import threading
-import  os
+import os
 
-
+# Fonction pour recevoir les messages du serveur
 def receive_messages(client_socket):
     try:
         while True:
+            # Recevoir le message du serveur
             message = client_socket.recv(128).decode('utf-8')
+            # Vérifier si le message est vide (connexion fermée par le serveur)
             if not message:
                 break
+            # Afficher le message reçu
             print(message)
     except Exception as e:
+        # Gérer les erreurs de réception
         print(f"Erreur de réception : {e}")
     finally:
+        # Fermer le socket client
         client_socket.close()
         print("La connexion au serveur a été fermée.")
+        # Terminer le programme
         os._exit(0)
 
-
-
+# Créer un socket client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
+# Définir l'hôte et le port du serveur auquel le client se connectera
 host, port = "127.0.0.1", 9999
 
-
+# Se connecter au serveur
 client.connect((host, port))
 
-
+# Demander le nom de l'utilisateur
 nom = input('Quel est votre nom ? ')
+# Envoyer le nom au serveur
 client.send(nom.encode('utf-8'))
 
-
+# Envoyer un message indiquant que l'utilisateur a rejoint le chatroom
 client.send(f"{nom} a rejoint le chatroom.".encode('utf-8'))
 
-
+# Créer un thread pour recevoir les messages du serveur 
 thread_receive = threading.Thread(target=receive_messages, args=(client,))
 thread_receive.start()
 
-
+# Boucle principale pour envoyer des messages au serveur
 while True:
+    # Demander à l'utilisateur d'entrer un message
     message = input(f"{nom} > ")
+    # Envoyer le message au serveur
     client.send(f"{nom} > {message}".encode('utf-8'))
-
-
-
--- TP3/Serveur.py
+++ TP3/Serveur.py
@@ -1,61 +1,75 @@
 import socket
 import select
 
-
+# Créer un objet socket pour le serveur
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
+# Définir l'hôte et le port sur lequel le serveur écoutera
 host, port = "127.0.0.1", 9999
 
-
+# Lier le serveur à l'hôte et au port spécifiés
 server.bind((host, port))
+# Définir le nombre maximal de connexions en attente
 server.listen(5)
 
-
-
+# Créer une liste contenant le socket du serveur
 socket_objs = [server]
 
+# Afficher un message de bienvenue
 print("Bienvenue dans le chatroom !!!")
 
-
-# Ajouter un dictionnaire pour stocker les noms associés aux sockets
+# Ajouter un dictionnaire pour stocker les noms associés aux sockets clients
 client_noms = {}
 
+# Fonction pour diffuser un message à tous les clients sauf à l'émetteur
 def broadcast(server_socket, sender_socket, message):
     for socket_obj in socket_objs:
         if socket_obj != server_socket and socket_obj != sender_socket:
             try:
                 socket_obj.send(f"{message}".encode('utf-8'))
             except:
+                # Gérer les erreurs de connexion et supprimer le socket défectueux
                 socket_objs.remove(socket_obj)
 
-
+# Boucle principale du serveur
 while True:
+    # Utiliser select pour surveiller les sockets en lecture
     list_lue, list_accee_Ecrit, exception = select.select(
         socket_objs, [], socket_objs)
 
+    # Parcourir les sockets prêts à être lus
     for socket_obj in list_lue:
+        # Vérifier si le socket en cours est le socket du serveur
         if socket_obj is server:
-
+            # Accepter la connexion du client
             client, address = server.accept()
+            # Ajouter le socket client à la liste des sockets
             socket_objs.append(client)
             # Demander le nom au client
             client_name = client.recv(128).decode('utf-8')
+            # Associer le nom du client à son socket dans le dictionnaire
             client_noms[client] = client_name
 
-            print(f"Nouveau participant {client_name} connecte depuis {address}")
+            # Afficher un message indiquant qu'un nouveau participant a rejoint le chatroom
+            print(f"Nouveau participant {client_name} connecté depuis {address}")
+            # Diffuser le message à tous les clients
             broadcast(server, client, f"{client_name} a rejoint le chatroom.")
         else:
             try:
-                donnees_recus = socket_obj.recv(128).decode('utf-8')
+                # Recevoir des données du client
+                donnees_recues = socket_obj.recv(128).decode('utf-8')
 
-                if donnees_recus:
-                    print(donnees_recus)
-                    broadcast(server, socket_obj, donnees_recus)
+                if donnees_recues:
+                    # Afficher les données reçues
+                    print(donnees_recues)
+                    # Diffuser le message à tous les clients
+                    broadcast(server, socket_obj, donnees_recues)
             except ConnectionResetError:
-
+                # Gérer les erreurs de connexion et supprimer le socket défectueux
                 socket_objs.remove(socket_obj)
-                print(client_noms[socket_obj] +" s'est deconnecte")
-                broadcast(server, socket_obj, client_noms[socket_obj] + " s'est deconnecte.")
+                # Afficher un message indiquant qu'un client s'est déconnecté
+                print(client_noms[socket_obj] + " s'est déconnecté")
+                # Diffuser le message à tous les clients
+                broadcast(server, socket_obj, client_noms[socket_obj] + " s'est déconnecté.")
+                # Afficher le nombre de participants restants
                 print(f"{len(socket_objs) - 1} participants restants")
-
