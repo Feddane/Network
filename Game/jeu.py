f9973d0f8e6c91010ec08d9430c123af02493dbb
Feddane <chafeddane@gmail.com>
2023-11-27 11:02:48 +0100

    changes in game

--- Game/jeu.py
+++ Game/jeu.py
@@ -3,8 +3,6 @@ from random import randint
 #Liste des options
 jeu  = ["pierre", "papier", "ciseaux"]
 
-#Attribuer une option aléatoire à l'ordinateur
-ordinateur = jeu[randint(0,2)]
 
 #Gardez le compte des points
 Pointsjoueur = 0
@@ -14,6 +12,11 @@ continuer = True
 
 #La boucle continue jusqu'à ce que la variable continuer est fausse
 while(continuer):
+
+    #Attribuer une option aléatoire à l'ordinateur
+    ordinateur = jeu[randint(0,2)]
+
+
     #Demander le choix de l'ordinateur<br>
     joueur = input("pierre, papier, ciseaux? ou tapez Fin pour arrêter le jeu!\n")
 
@@ -31,22 +34,21 @@ while(continuer):
             Pointsjoueur = Pointsjoueur + 1
     elif(joueur == "papier"):
         if(ordinateur == "ciseaux"):
-            print("Perdu!", ordinateur, "cut", joueur)
+            print("Perdu!", ordinateur, "coupe", joueur)
             Pointsordinateur = Pointsordinateur + 1
         else:
-            print("You win!", joueur, "recouvre", ordinateur)
+            print("Gagné!", joueur, "recouvre", ordinateur)
             Pointsjoueur = Pointsjoueur + 1
     elif(joueur == "ciseaux"):
-        if(ordinateur == "Rock"):
-            print("Perdu...", ordinateur, "écrase", joueur)
+        if(ordinateur == "pierre"):
+            print("Perdu!", ordinateur, "écrase", joueur)
             Pointsordinateur = Pointsordinateur + 1
         else:
-            print("Gagné!", joueur, "cut", ordinateur)
+            print("Gagné!", joueur, "coupe", ordinateur)
             Pointsjoueur = Pointsjoueur + 1
     else:
         print("Votre choix n'est pas correct, vérifiez l'orthographe!")
-    #Attribuer une option aléatoire à l'ordinateur
-    ordinateur = jeu[randint(0,2)]
+    
     print('********Tour suivant********')
 
 #Impression des points
