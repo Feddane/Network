f68b5a3c2cc986e89182e2fcdc4da36c9f4e13bd
Feddane <chafeddane@gmail.com>
2023-12-09 21:26:39 +0100

    petit test

--- FinalGame/client.py
+++ FinalGame/client.py
@@ -3,95 +3,100 @@ import threading
 import pygame
 import sys
 
-# Client Constants
-HOST = '127.0.0.1'
-PORT = 5555
+# Define mole_positions as an empty list
+mole_positions = []
 
-# Game Constants
-WIDTH, HEIGHT = 600, 400
-FPS = 60
-MOLE_SIZE = 50
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+# Define mole_radius as a constant
+MOLE_RADIUS = 30
 
-# Client variables
-client_id = 0
+# Define a variable to track the player's score
+player_score = 0
 
-def receive_mole_coordinates(client_socket):
-    global client_id
+def pygame_thread():
+    global player_names, current_round, score_player1, score_player2, player_score
 
-    while True:
-        try:
-            data = client_socket.recv(1024).decode('utf-8')
-            if not data:
-                break
-
-            x, y, client_id = map(int, data.split(','))
-            return x, y
-
-        except Exception as e:
-            print(f"Error receiving mole coordinates: {e}")
-            break
-
-def send_whack_event(client_socket):
-    try:
-        client_socket.send("whack".encode('utf-8'))
-    except Exception as e:
-        print(f"Error sending whack event: {e}")
-
-def draw_mole(screen, x, y):
-    pygame.draw.circle(screen, RED, (x, y), MOLE_SIZE)
-
-def show_score(screen, score):
-    font = pygame.font.Font(None, 36)
-    score_text = font.render(f"Score: {score}", True, WHITE)
-    screen.blit(score_text, (10, 10))
-
-def game_over(screen, final_score):
-    font = pygame.font.Font(None, 72)
-    game_over_text = font.render("Game Over", True, RED)
-    screen.blit(game_over_text, (WIDTH // 2 - 150, HEIGHT // 2 - 50))
-
-    final_score_text = font.render(f"Final Score: {final_score}", True, RED)
-    screen.blit(final_score_text, (WIDTH // 2 - 180, HEIGHT // 2 + 50))
-
-    pygame.display.flip()
-    pygame.time.wait(3000)
-    pygame.quit()
-    sys.exit()
-
-def main():
-    global client_id
-
+    # Pygame initialization code
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Whack-a-Mole Client")
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Whack-a-Mole")
 
     clock = pygame.time.Clock()
 
     while True:
-        screen.fill(WHITE)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                client_socket.close()
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                send_whack_event(client_socket)
 
-        x, y = receive_mole_coordinates(client_socket)
-        draw_mole(screen, x, y)
-        show_score(screen, client_id)
+            # Check for mouse clicks
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for mole_pos in mole_positions:
+                    mole_x, mole_y = mole_pos
+                    # Check if the mouse click is inside the mole
+                    if (
+                        mole_x - MOLE_RADIUS <= mouse_x <= mole_x + MOLE_RADIUS
+                        and mole_y - MOLE_RADIUS <= mouse_y <= mole_y + MOLE_RADIUS
+                    ):
+                        # If the player successfully whacks the mole, increment the score
+                        player_score += 1
+                        print(f"{user_name} whacked a mole! Score: {player_score}")
+
+        screen.fill((255, 255, 255))
+
+        # Pygame Whack-a-Mole game logic
+        for mole_pos in mole_positions:
+            pygame.draw.circle(screen, (255, 0, 0), mole_pos, MOLE_RADIUS)
+
+        # Display the player's score
+        font = pygame.font.Font(None, 36)
+        score_text = font.render(f"Score: {player_score}", True, (0, 0, 0))
+        screen.blit(score_text, (10, 10))
 
         pygame.display.flip()
-        clock.tick(FPS)
+        clock.tick(60)
 
-if __name__ == "__main__":
-    main()
+# Start the Pygame thread
+pygame_thread = threading.Thread(target=pygame_thread)
+pygame_thread.start()
+
+# Connect to the server
+host = '127.0.0.1'
+port = 12347
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+# Get player name from the user
+user_name = input("Enter your name: ")
+client.send(user_name.encode('utf-8'))
+
+try:
+    while True:
+        # Receive mole information from the server
+        data = client.recv(1024).decode('utf-8')
+        if data.startswith("MOLE"):
+            _, mole_x, mole_y = data.split()
+            mole_x, mole_y = int(mole_x), int(mole_y)
+
+            # Add mole position to the mole_positions list
+            mole_positions.append((mole_x, mole_y))
+
+            # Handle mole rendering (not included in this response for brevity)
+            # You need to implement the logic to render the mole on the client side
+
+        elif data == "GAME_OVER":
+            print("Game over. Final score:", player_score)
+            break
+
+        # Handle player's whack (not included in this response for brevity)
+        # You need to implement the logic to handle the player's whack
+
+except KeyboardInterrupt:
+    print("Game interrupted.")
+finally:
+    client.close()
--- FinalGame/server.py
+++ FinalGame/server.py
@@ -3,75 +3,77 @@ import threading
 import random
 import time
 
-# Server Constants
-HOST = '127.0.0.1'
-PORT = 5555
+def handle_client(conn, player, mole_callback):
+    global current_round, score_player1, score_player2
 
-# Game Constants
-MOLE_APPEAR_TIME = 2  # in seconds
-GAME_DURATION = 30  # in seconds
-
-# Server variables
-moles = []
-last_mole_time = 0
-scoreboard = {}
-
-def handle_client(client, client_address, client_id):
-    global moles, last_mole_time, scoreboard
-
-    print(f"[*] Accepted connection from {client_address}")
+    # Receive the player's name
+    player_name = conn.recv(1024).decode('utf-8')
+    print(f"{player_name} has joined the game.")
 
     while True:
-        try:
-            data = client.recv(1024).decode('utf-8')
-            if not data:
-                break
+        # Whack-a-Mole game logic
+        time.sleep(2)  # Wait for 2 seconds before creating a new mole
 
-            if data == "whack":
-                current_time = time.time()
-                if current_time - last_mole_time > MOLE_APPEAR_TIME:
-                    moles.pop(0) if moles else None  # Whack the mole if there's one, else do nothing
-                    last_mole_time = current_time
+        mole_x = random.randint(50, 750)
+        mole_y = random.randint(50, 550)
 
-                    if client_id not in scoreboard:
-                        scoreboard[client_id] = 1
-                    else:
-                        scoreboard[client_id] += 1
+        mole_callback((mole_x, mole_y))
 
-        except Exception as e:
-            print(f"Error with client {client_id}: {e}")
-            break
+        # Handle player's whack (not included in this response for brevity)
+        # You need to implement the logic to handle the player's whack
 
-    print(f"[*] Connection from {client_address} closed")
-    client.close()
+    # Close the connection when the player leaves
+    print(f"{player_name} has left the game.")
+    conn.close()
 
-def generate_mole():
-    x, y = random.randint(50, 550), random.randint(50, 350)
-    return x, y
+# Server initialization code
+host = '127.0.0.1'
+port = 12347
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(2)
 
-    print(f"[*] Server is listening on {HOST}:{PORT}")
+print("Welcome to the Whack-a-Mole game!")
+print(f"Server listening on {host}:{port}")
 
-    while True:
-        client, address = server.accept()
-        client_id = random.randint(1, 1000)
+# Initialize game state variables
+player_names = []
+current_round = 0
+score_player1 = 0
+score_player2 = 0
 
-        # Initialize client's score
-        scoreboard[client_id] = 0
+# Create a list to store mole positions
+mole_positions = []
 
-        # Generate a mole for the client
-        moles.append(generate_mole())
+def mole_callback(position):
+    mole_positions.append(position)
 
-        # Send the mole coordinates and client ID to the client
-        client.send(f"{moles[0][0]},{moles[0][1]},{client_id}".encode('utf-8'))
 
-        # Start a thread to handle the client
-        client_handler = threading.Thread(target=handle_client, args=(client, address, client_id))
-        client_handler.start()
+# Initialize a variable to control the game state
+game_running = True
 
-if __name__ == "__main__":
-    start_server()
+def send_mole_positions():
+    global game_running
+    while game_running:
+        time.sleep(5)  # Wait for 5 seconds before sending mole positions
+        for conn in clients:
+            mole_x = random.randint(50, 750)
+            mole_y = random.randint(50, 550)
+            message = f"MOLE {mole_x} {mole_y}"
+            conn.send(message.encode('utf-8'))
+
+    # Game over message
+    for conn in clients:
+        conn.send("GAME_OVER".encode('utf-8'))
+
+# Start a separate thread to send mole positions to clients
+threading.Thread(target=send_mole_positions).start()
+
+# List to store client connections
+clients = []
+
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    threading.Thread(target=handle_client, args=(conn, len(clients), mole_callback)).start()
