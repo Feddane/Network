46238bc7b0171c8aca814cc4090b5f7c8eb9e3cc
Feddane <chafeddane@gmail.com>
2023-12-12 23:02:53 +0100

    tentative 1 du jeu tic-tac-toe

--- FinalGame/client.py
+++ FinalGame/client.py
@@ -1,95 +1,287 @@
 import socket
 import threading
-import pickle
-import pygame
 
-# Client configuration
-SERVER = '127.0.0.1'
-PORT = 5555
+from tkinter import * 
+from tkinter import messagebox
 
-# Initialize Pygame
-pygame.init()
+window = Tk()
+cell = ""
+turn = False
+host = "127.0.0.1"
+port = 65535
 
-# Pygame window settings
-WIDTH, HEIGHT = 800, 600
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Ping Pong Game")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
 
-# Colors
-WHITE = (255, 255, 255)
+def update():
+    if cell == 'A':
+        clicked1()
+    elif cell == 'B':
+        clicked2()
+    elif cell == 'C':
+        clicked3()
+    elif cell == 'D':
+        clicked4()
+    elif cell == 'E':
+        clicked5()
+    elif cell == 'F':
+        clicked6()
+    elif cell == 'G':
+        clicked7()
+    elif cell == 'H':
+        clicked8()
+    elif cell == 'I':
+        clicked9()
+    else:
+        print("no matching char detected")
 
-# Paddle dimensions
-PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+def create_thread(target):
+    thread = threading.Thread(target=target)
+    thread.daemon = True        #deamon threads are killed automatically when the program exits
+    thread.start()
 
-# Ball dimensions
-BALL_SIZE = 20
 
-# Client class to handle networking
-class Client:
-    def __init__(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((SERVER, PORT))
-        self.paddle1_pos = [50, 250]
-        self.paddle2_pos = [750, 250]
-        self.ball_pos = [400, 300]
-
-    def send_position(self):
-        pos_data = {"paddle1_pos": self.paddle1_pos, "paddle2_pos": self.paddle2_pos}
-        self.client_socket.send(pickle.dumps(pos_data))
-
-    def receive_data(self):
-        while True:
-            try:
-                data = pickle.loads(self.client_socket.recv(1024))
-                # Update game state based on received data
-                self.ball_pos = data["ball_pos"]
-                self.paddle1_pos = data["paddle1_pos"]
-                self.paddle2_pos = data["paddle2_pos"]
-            except:
-                break
-
-# Game loop
-def game_loop(client):
-    clock = pygame.time.Clock()
-
+def recieveData():
+    global cell
+    global turn
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+        data, addr =  sock.recvfrom(1024) #loop to recieve the adress and the message
+        data2 = data.decode()
+        dataa = data2.split('-')
+        cell = dataa[0]
+        update()
+        if dataa[1] == 'YourTurn':
+            print(cell + " Client turn")
+            turn = True
+            print(" Client turn = "+ str(turn))
 
-        keys = pygame.key.get_pressed()
-        # Update player position based on key presses
-        if keys[pygame.K_w] and client.paddle1_pos[1] > 0:
-            client.paddle1_pos[1] -= 5
-        if keys[pygame.K_s] and client.paddle1_pos[1] < HEIGHT - PADDLE_HEIGHT:
-            client.paddle1_pos[1] += 5
+create_thread(recieveData)
 
-        # Update player position based on key presses for paddle 2
-        if keys[pygame.K_UP] and client.paddle2_pos[1] > 0:
-            client.paddle2_pos[1] -= 5
-        if keys[pygame.K_DOWN] and client.paddle2_pos[1] < HEIGHT - PADDLE_HEIGHT:
-            client.paddle2_pos[1] += 5
+window.title("Welcome player 2 to the game Tic-Tac-Toe")
+window.geometry("400x300")
 
-        # Send player position to the server
-        client.send_position()
 
-        # Draw paddles and ball on the screen
-        win.fill(WHITE)
-        pygame.draw.rect(win, (0, 128, 255), (client.paddle1_pos[0], client.paddle1_pos[1], PADDLE_WIDTH, PADDLE_HEIGHT))
-        pygame.draw.rect(win, (255, 0, 0), (client.paddle2_pos[0], client.paddle2_pos[1], PADDLE_WIDTH, PADDLE_HEIGHT))
-        pygame.draw.ellipse(win, (255, 255, 0), (client.ball_pos[0], client.ball_pos[1], BALL_SIZE, BALL_SIZE))
-        pygame.display.update()
+host = '127.0.0.2'
+port = 65432
 
-        clock.tick(60)
+lbl = Label(window, text="Tic-Tac-Toe Game", font=('Helvetica', 15))
+lbl.grid(row=0, column=0)
+lbl = Label(window, text="Player 1: X", font=('Helvetica', 10))
+lbl.grid(row=1, column=0)
+lbl = Label(window, text="Player 2: O", font=('Helvetica', 10))
+lbl.grid(row=2, column=0)
 
-# Run the client
-if __name__ == "__main__":
-    client = Client()
+#for server turn
+print(" client turn = "+ str(turn))
 
-    # Start a thread to handle data reception from the server
-    receive_thread = threading.Thread(target=client.receive_data)
-    receive_thread.start()
+def clicked1():
+    global turn
+    global cell
+    if turn == True and btn1["text"] == " ":
+        btn1["text"] = "O"
+        send_data = '{}-{}'.format('A', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and cell == 'A':
+        btn1["text"] = "X"
+        turn = True
+        check()
 
-    # Run the game loop
-    game_loop(client)
+
+def clicked2():
+    global turn
+    global cell
+    if turn == True and btn2["text"] == " " and btn2["text"] == " ":
+        btn2["text"] = "O"
+        send_data = '{}-{}'.format('B', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn2["text"] == " " and cell == 'B':
+        btn2["text"] = "X"
+        turn = True
+        check()
+
+def clicked3():
+    global turn
+    global cell
+    if turn == True and btn3["text"] == " ":
+        btn3["text"] = "O"
+        send_data = '{}-{}'.format('C', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn3["text"] == " " and cell == 'C':
+        btn3["text"] = "X"
+        turn = True
+        check()
+
+
+def clicked4():
+    global turn
+    global cell
+    if turn == True and btn4["text"] == " ":
+        btn4["text"] = "O"
+        send_data = '{}-{}'.format('D', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn4["text"] == " " and cell == 'D':
+        btn4["text"] = "X"
+        turn = True
+        check()
+
+
+def clicked5():
+    global turn
+    global cell
+    if turn == True and btn5["text"] == " ":
+        btn5["text"] = "O"
+        send_data = '{}-{}'.format('E', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn5["text"] == " " and cell == 'E':
+        btn5["text"] = "X"
+        turn = True
+        check()
+
+def clicked6():
+    global turn
+    global cell
+    if turn == True and btn6["text"] == " ":
+        btn6["text"] = "O"
+        send_data = '{}-{}'.format('F', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn6["text"] == " " and cell == 'F':
+        btn6["text"] = "X"
+        turn = True
+        check()
+
+def clicked7():
+    global turn
+    global cell
+    if turn == True and btn7["text"] == " ":
+        btn7["text"] = "O"
+        send_data = '{}-{}'.format('G', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn7["text"] == " " and cell == 'G':
+        btn7["text"] = "X"
+        turn = True
+        check()
+
+
+def clicked8():
+    global turn
+    global cell
+    if turn == True and btn8["text"] == " ":
+        btn8["text"] = "O"
+        send_data = '{}-{}'.format('H', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn8["text"] == " " and cell == 'H':
+        btn8["text"] = "X"
+        turn = True
+        check()
+
+
+def clicked9():
+    global turn
+    global cell
+    if turn == True and btn9["text"] == " ":
+        btn9["text"] = "O"
+        send_data = '{}-{}'.format('I', 'YourTurn').encode()
+        sock.send(send_data)
+        print(send_data)
+        turn = False
+        print(" Server turn = "+ str(turn))
+        check()
+    elif turn == False and btn9["text"] == " " and cell == 'I':
+        btn9["text"] = "X"
+        turn = True
+        check()
+
+flag = 1
+def check():    #check if a win case exists
+    global flag
+    b1 = btn1["text"]       #get text in the button
+    b2 = btn2["text"]
+    b3 = btn3["text"]
+    b4 = btn4["text"]
+    b5 = btn5["text"]
+    b6 = btn6["text"]
+    b7 = btn7["text"]
+    b8 = btn8["text"]
+    b9 = btn9["text"]
+    flag = flag + 1
+
+    if b1 == b2 and b1 == b3 and b1 == "O" or  b1 == b2 and b1 == b3 and b1 == "X":
+        win(btn1["text"])
+    if b4 == b5 and b4 == b6 and b4 == "O" or  b4 == b5 and b4 == b6 and b4 == "X":
+        win(btn4["text"])
+    if b7 == b8 and b7 == b9 and b7 == "O" or  b7 == b8 and b7 == b9 and b7 == "X":
+        win(btn7["text"])
+    if b1 == b4 and b1 == b7 and b1 == "O" or  b1 == b4 and b1 == b7 and b1 == "X":
+        win(btn1["text"])
+    if b2 == b5 and b2 == b8 and b2 == "O" or  b2 == b5 and b2 == b8 and b2 == "X":
+        win(btn2["text"])
+    if b3 == b6 and b3 == b9 and b3 == "O" or  b3 == b6 and b3 == b9 and b3 == "X":
+        win(btn3["text"])
+    if b1 == b5 and b1 == b9 and b1 == "O" or  b1 == b5 and b1 == b9 and b1 == "X":
+        win(btn1["text"])
+    if b7 == b5 and b7 == b3 and b7 == "O" or  b7 == b5 and b7 == b3 and b7 == "X":
+        win(btn7["text"])
+    if flag == 10:
+        messagebox.showinfo("Tie", "Match Tied!! Try again :)")
+        window.destroy()
+
+
+def win(player):
+    ans = "Game complete " + player + " wins"
+    messagebox.showinfo("Congratulations", ans)
+    window.destroy() #is used to clos the program
+
+#create the buttons for the game
+btn1 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked1)
+btn1.grid(column=1, row=1)
+
+btn2 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked2)
+btn2.grid(column=2, row=1)
+
+btn3 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked3)
+btn3.grid(column=3, row=1)
+
+btn4 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked4)
+btn4.grid(column=1, row=2)
+
+btn5 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked5)
+btn5.grid(column=2, row=2)
+
+
+btn6 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked6)
+btn6.grid(column=3, row=2)
+
+btn7 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked7)
+btn7.grid(column=1, row=3)
+
+btn8 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked8)
+btn8.grid(column=2, row=3)
+
+btn9 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked9)
+btn9.grid(column=3, row=3)
+
+window.mainloop()
--- FinalGame/server.py
+++ FinalGame/server.py
@@ -1,59 +1,298 @@
 import socket
 import threading
-import pickle
 
-# Server configuration
-HOST = '127.0.0.1'
-PORT = 5555
+from tkinter import *
+from tkinter import messagebox
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+window = Tk()
 
-print(f"Server listening on {HOST}:{PORT}")
+cell = ''
+turn = True
 
-# Store connected clients
-clients = []
+host = '127.0.0.1'
+port = 65535
 
-# Game state
-ball_pos = [400, 300]
-paddle1_pos = [50, 250]
-paddle2_pos = [750, 250]
+conn, adr = None, None
 
-# Ball speed
-ball_speed = [5, 5]
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(1)
 
-# Broadcast message to all clients
-def broadcast(message, sender):
-    for client in clients:
-        if client != sender:
-            try:
-                client.send(pickle.dumps(message))
-            except:
-                # Remove disconnected client
-                clients.remove(client)
-
-# Handle client connections
-def handle_client(client):
-    global ball_pos, paddle1_pos, paddle2_pos
-    while True:
-        try:
-            data = pickle.loads(client.recv(1024))
-            paddle1_pos = data["paddle1_pos"]
-            paddle2_pos = data["paddle2_pos"]
-            broadcast({"ball_pos": ball_pos, "paddle1_pos": paddle1_pos, "paddle2_pos": paddle2_pos}, client)
-        except:
-            # Remove disconnected client
-            clients.remove(client)
-            break
-
-# Accept and handle client connections
-while True:
-    client, address = server_socket.accept()
-    clients.append(client)
-    print(f"Connection established with {address}")
-    
-    # Start a new thread for each client
-    thread = threading.Thread(target=handle_client, args=(client,))
+def create_thread(target):
+    thread = threading.Thread(target=target)
+    thread.daemon = True        #deamon threads are killed automatically when the program exits
     thread.start()
+
+def recieveData():
+    global cell
+    global turn
+    while True:
+        data, addr =  conn.recvfrom(1024) #loop to recieve the adress and the message
+        data2 = data.decode()
+        dataa = data2.split('-')
+        cell = dataa[0]
+        update()
+        if dataa[1] == 'YourTurn':
+            turn = True
+            print(" server turn = "+ str(turn))
+
+
+
+def update():
+    if cell == 'A':
+        clicked1()
+    elif cell == 'B':
+        clicked2()
+    elif cell == 'C':
+        clicked3()
+    elif cell == 'D':
+        clicked4()
+    elif cell == 'E':
+        clicked5()
+    elif cell == 'F':
+        clicked6()
+    elif cell == 'G':
+        clicked7()
+    elif cell == 'H':
+        clicked8()
+    elif cell == 'I':
+        clicked9()
+    else:
+        print("no matching char detected")
+
+
+
+def waitingforconnection():
+    print("thread created")
+    global conn, addr
+    conn, addr =  sock.accept()
+    print("Client is connected")
+    recieveData()
+
+create_thread(waitingforconnection)
+
+
+
+window.title("Welcome player 1 to the game Tic-Tac-Toe")
+
+window.geometry("400x300")
+
+lbl = Label(window, text="Tic-Tac-Toe Game", font=('Helvetica', 15))
+lbl.grid(row=0, column=0)
+lbl = Label(window, text="Player 1: X", font=('Helvetica', 10))
+lbl.grid(row=1, column=0)
+lbl = Label(window, text="Player 2: O", font=('Helvetica', 10))
+lbl.grid(row=2, column=0)
+
+def clicked1():
+    global turn 
+    global cell
+    if turn == True and btn1["text"] == " ":
+        btn1["text"] = "X"
+        send_data = '{}-{}'.format('A', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and cell == 'A':
+        btn1["text"] = "O"
+        turn = True
+        check()
+
+
+def clicked2():
+    global turn
+    global cell
+    if turn == True and btn2["text"] == " " and btn2["text"] == " ":
+        btn2["text"] = "X"
+        send_data = '{}-{}'.format('B', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn2["text"] == " " and cell == 'B':
+        btn2["text"] = "O"
+        turn = True
+        check()
+
+def clicked3():
+    global turn
+    global cell
+    if turn == True and btn3["text"] == " ":
+        btn3["text"] = "X"
+        send_data = '{}-{}'.format('C', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn3["text"] == " " and cell == 'C':
+        btn3["text"] = "O"
+        turn = True
+        check()
+
+
+def clicked4():
+    global turn
+    global cell
+    if turn == True and btn4["text"] == " ":
+        btn4["text"] = "X"
+        send_data = '{}-{}'.format('D', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn4["text"] == " " and cell == 'D':
+        btn4["text"] = "O"
+        turn = True
+        check()
+
+
+def clicked5():
+    global turn
+    global cell
+    if turn == True and btn5["text"] == " ":
+        btn5["text"] = "X"
+        send_data = '{}-{}'.format('E', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn5["text"] == " " and cell == 'E':
+        btn5["text"] = "O"
+        turn = True
+        check()
+
+def clicked6():
+    global turn
+    global cell
+    if turn == True and btn6["text"] == " ":
+        btn6["text"] = "X"
+        send_data = '{}-{}'.format('F', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn6["text"] == " " and cell == 'F':
+        btn6["text"] = "O"
+        turn = True
+        check()
+
+def clicked7():
+    global turn
+    global cell
+    if turn == True and btn7["text"] == " ":
+        btn7["text"] = "X"
+        send_data = '{}-{}'.format('G', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn7["text"] == " " and cell == 'G':
+        btn7["text"] = "O"
+        turn = True
+        check()
+
+
+def clicked8():
+    global turn
+    global cell
+    if turn == True and btn8["text"] == " ":
+        btn8["text"] = "X"
+        send_data = '{}-{}'.format('H', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        check()
+    elif turn == False and btn8["text"] == " " and cell == 'H':
+        btn8["text"] = "O"
+        turn = True
+        check()
+
+
+def clicked9():
+    global turn
+    global cell
+    if turn == True and btn9["text"] == " ":
+        btn9["text"] = "X"
+        send_data = '{}-{}'.format('I', 'YourTurn').encode()
+        conn.send(send_data)
+        print(send_data)
+        turn = False
+        print(" Server turn = "+ str(turn))
+        check()
+    elif turn == False and btn9["text"] == " " and cell == 'I':
+        btn9["text"] = "O"
+        turn = True
+        check()
+
+flag = 1
+def check():    #check if a win case exists
+    global flag
+    b1 = btn1["text"]       #get text in the button
+    b2 = btn2["text"]
+    b3 = btn3["text"]
+    b4 = btn4["text"]
+    b5 = btn5["text"]
+    b6 = btn6["text"]
+    b7 = btn7["text"]
+    b8 = btn8["text"]
+    b9 = btn9["text"]
+    flag = flag + 1
+
+    if b1 == b2 and b1 == b3 and b1 == "O" or  b1 == b2 and b1 == b3 and b1 == "X":
+        win(btn1["text"])
+    if b4 == b5 and b4 == b6 and b4 == "O" or  b4 == b5 and b4 == b6 and b4 == "X":
+        win(btn4["text"])
+    if b7 == b8 and b7 == b9 and b7 == "O" or  b7 == b8 and b7 == b9 and b7 == "X":
+        win(btn7["text"])
+    if b1 == b4 and b1 == b7 and b1 == "O" or  b1 == b4 and b1 == b7 and b1 == "X":
+        win(btn1["text"])
+    if b2 == b5 and b2 == b8 and b2 == "O" or  b2 == b5 and b2 == b8 and b2 == "X":
+        win(btn2["text"])
+    if b3 == b6 and b3 == b9 and b3 == "O" or  b3 == b6 and b3 == b9 and b3 == "X":
+        win(btn3["text"])
+    if b1 == b5 and b1 == b9 and b1 == "O" or  b1 == b5 and b1 == b9 and b1 == "X":
+        win(btn1["text"])
+    if b7 == b5 and b7 == b3 and b7 == "O" or  b7 == b5 and b7 == b3 and b7 == "X":
+        win(btn7["text"])
+    if flag == 10:
+        messagebox.showinfo("Tie", "Match Tied!! Try again :)")
+        window.destroy()
+
+
+def win(player):
+    ans = "Game complete " + player + " wins"
+    messagebox.showinfo("Congratulations", ans)
+    window.destroy() #is used to clos the program
+
+
+#create the buttons for the game
+btn1 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked1)
+btn1.grid(column=1, row=1)
+
+btn2 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked2)
+btn2.grid(column=2, row=1)
+
+btn3 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked3)
+btn3.grid(column=3, row=1)
+
+btn4 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked4)
+btn4.grid(column=1, row=2)
+
+btn5 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked5)
+btn5.grid(column=2, row=2)
+
+
+btn6 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked6)
+btn6.grid(column=3, row=2)
+
+btn7 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked7)
+btn7.grid(column=1, row=3)
+
+btn8 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked8)
+btn8.grid(column=2, row=3)
+
+btn9 = Button(window, text= " ", bg="white", fg="black", width=3, height=1, font=('Helvetica', 20), command=clicked9)
+btn9.grid(column=3, row=3)
+
+window.mainloop()
