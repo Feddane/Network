e10a1c41e1304e5d13581262ac342d5b53548f64
Feddane <chafeddane@gmail.com>
2023-12-17 16:18:43 +0100

    add threading dans serveur

--- Guess-The-Number/serveur.py
+++ Guess-The-Number/serveur.py
@@ -1,42 +1,54 @@
-import socket       # Modules utilisés
+import socket
+import threading
 import random
 
-serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Création du socket
-serveur.bind(("127.0.0.1", 8001))  # Liaison du socket à l'adresse IP locale et au port 8000
+def handle_client(client, user_name):
+    num = random.randint(1, 20)
+    print(f"Le nombre aléatoire est {num} pour {user_name}")
 
-while True:
-    serveur.listen(1)   # Attente d'une connexion (1 connexion à la fois)
-    print("Attente de connexion...")
-    (client, (ip, port)) = serveur.accept()   # Acceptation de la connexion
-    
-    # Recevoir le nom du joueur
-    user_name = client.recv(1024).decode()
-    print(f"{user_name} s'est connecté")  # Affiche le nom du joueur qui s'est connecté et le message
-    
-    num = random.randint(1, 20)   # Génération d'un nombre aléatoire entre 1 et 20
-    print("Le nombre aléatoire est ", num)
     turn = 0
-
-    while turn <= 5:   # Logique du jeu
+    while turn <= 5:
         try:
             turn += 1
-            data = int(client.recv(2048).decode())   # Réception des données du client
-            if int(data) == num:
-                client.send("Win".encode() + str(turn).encode())   # Envoi du message "Win" et du nombre de tours
+            data = int(client.recv(2048).decode())
+
+            if data == num:
+                client.send("Win".encode() + str(turn).encode())
                 break
-            if int(data) > num:
+            elif data > num:
                 if turn == 5:
-                    client.send("Lost".encode() + str(turn).encode())   # Envoi du message "Lost" et du nombre de tours
+                    client.send("Lost".encode() + str(turn).encode())
                     break
                 else:
-                    client.send("High".encode() + str(turn).encode())   # Envoi du message "High" et du nombre de tours
-            if int(data) < num:
+                    client.send("High".encode() + str(turn).encode())
+            elif data < num:
                 if turn == 5:
-                    client.send("Lost".encode() + str(turn).encode())   # Envoi du message "Lost" et du nombre de tours
+                    client.send("Lost".encode() + str(turn).encode())
                     break
                 else:
-                    client.send("Low".encode() + str(turn).encode())   # Envoi du message "Low" et du nombre de tours
+                    client.send("Low".encode() + str(turn).encode())
+
         except ValueError:
             break
 
-    print("Nombre de tours effectués ", turn)
+    print(f"Nombre de tours effectués pour {user_name}: {turn}")
+    client.close()
+
+def main():
+    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serveur.bind(("127.0.0.1", 8001))
+    serveur.listen(5)  # Accepte jusqu'à 5 connexions simultanées
+
+    print("Attente de connexions...")
+
+    while True:
+        client, (ip, port) = serveur.accept()
+        user_name = client.recv(1024).decode()
+        print(f"{user_name} s'est connecté depuis {ip}:{port}")
+
+        # Crée un thread pour gérer le client actuel
+        client_handler = threading.Thread(target=handle_client, args=(client, user_name))
+        client_handler.start()
+
+if __name__ == "__main__":
+    main()
